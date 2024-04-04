f2b154d0993947987f92483fde4fcdbd667701f0
ferrytm <164608041+ferrytm@users.noreply.github.com>
2024-04-05 02:53:39 +0700

    load

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 
 df = pd.read_csv("./datasource/spotify_top_songs_audio_features.csv")
 
-engine = create_engine('mysql://root:henshin@localhost/df_data')
+engine = create_engine('mysql://root:henshin@10.108.2.9/mysql')
 
 
 logging.basicConfig(filename='./docs/song_recommended_api.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -14,7 +14,7 @@ logging.basicConfig(filename='./docs/song_recommended_api.log', level=logging.IN
 logging.info('Loading output data to mysql table')
 start_time = time.time()
 try:
-    df.to_sql('test_load2', con=engine, if_exists='replace', index=False)
+    df.to_sql('spotify_top_songs', con=engine, if_exists='replace', index=False)
     logging.info('Data successfully written to MySQL table')
     end_time = time.time()
     elapsed_time = end_time - start_time
